26f3d151a5c5d394a9d4c0d857fccf520beb03ed
Kushaal <kushaalsm97@gmail.com>
2021-11-13 22:40:17 -0500

    Orderpage updated

---
+++ src/orderbook.py
@@ -0,0 +1,161 @@
+import datetime as dt
+from typing import Type
+
+class OrderBook:
+    def __init__(self, instrument_name) -> None:
+        self.bid_order_page = None
+        self.ask_order_page = None
+
+    def initialize_orderbook(self):
+        self.bid_order_page = OrderPage('BID')
+        self.ask_order_page = OrderPage('ASK')
+        
+        return
+
+class OrderPage:
+    """
+    This holds all the order levels for bid/ask sorted in price priority.
+    """
+    def __init__(self, page_type):
+        # page_type can be one of two values : BID or ASK
+        self.page_type = page_type
+        assert self.page_type in ['ASK', 'BID']
+        
+        # Dictionary containing the orderlevels(value) for the prices(key).
+        self.order_level_dict = {}
+
+        # Holds the best price for the orderpage.
+        self.best_price_level = None
+    
+    def _update_best_level(self):
+        self.best_price_level = (min if self.page_type == 'ASK' else max)(self.order_level_dict)
+        return
+    
+    def on_new_order(self, order):
+        order_price = order.price
+        if order.order_purpose == 'NEW':
+            if not order_price in self.order_level_dict:
+                self._add_order_level(order_price, order)
+            self.order_level_dict[order_price].add_order(order)
+        elif order.order_purpose == 'CANCEL':
+            if not order_price in self.order_level_dict:
+                raise Exception(f"Order level for the price {order_price} not present in the orderbook.")
+            self.order_level_dict[order_price].delete_order(order.order_id)
+        return
+
+    def _add_order_level(self, price, order):
+        self.order_level_dict[price] = OrderLevel(order)
+        return
+
+    def _delete_order_level(self):
+        return
+
+class OrderLevel:
+    """
+    This is one level in the orderbook. This has the list of all orders with the same direction and price.
+    The orders are sorted based on the order placement times.
+    All new orders are appended to the list and filled orders are popped from the beginning of the list.
+    """
+    def __init__(self, order) -> None:
+        # List of Order objects sorted by arrival_time.
+        self.order_list = [order]
+
+        # Set the price of this orderlevel.
+        self.price = order.price
+
+        # Total quantity of all orders at this price level.
+        self.combined_quantity = order.display_quantity
+
+        # Number of different orders at this price level.
+        self.number_of_orders = 1
+    
+    def add_order(self, order):
+        """
+        Update the order_list with the new order.
+        Update the combined_quantity at this price level.
+        """
+        self.order_list.append(order)
+        self.number_of_orders += 1
+        self._update_quantity(order.display_quantity)
+        return
+    
+    def delete_order(self, order_id):
+        """
+        Remove the order corresponding to the order_id and update the combined quantity at this level.
+        """
+        idx = 0
+        order_list_length = len(self.order_list)
+        order_found = False
+        while idx < order_list_length:
+            if self.order_list[idx].order_id == order_id:
+                order_found = True
+                # Argument passed is negative in order to decrement the combined_quantity
+                self._update_quantity(-self.order_list[idx].display_quantity)
+                self.order_list.pop(idx)
+                self.number_of_orders -= 1
+                break
+        if not order_found:
+            # raise Exception(f"Order with the order_id {order_id} not present.")
+            pass
+        return
+
+    def _update_quantity(self, updated_quantity):
+        self.combined_quantity += updated_quantity
+        return
+
+    def fill_quantity(self, quantity_to_match):
+        """
+        Distribute the quantity_to_match among the orders in the queue.
+        """
+        # Step 1 : Fill the orders.
+        for order in self.order_list:
+            fill_quantity = min(quantity_to_match, order.display_quantity)
+            quantity_to_match -= fill_quantity
+            order._update_filled_quantity(fill_quantity)
+            if quantity_to_match == 0:
+                break
+        # Post order fill tasks.
+        self._on_fill()
+
+        return
+    
+    def _on_fill(self):
+        """
+        2 tasks must be performed whenever there is an order fill.
+        1) Completely filled orders must be removed from the order_list at this level.
+        2) If an order isn't completely filled, the remaining quantity must be released and the order must be pushed back to the 
+           end of the order_list.
+        """
+        order_list_length = len(self.order_list)
+        idx = 0
+        while idx < order_list_length:
+            if self.order_list[idx]._is_filled():
+                # If the order is completely filled, pop out the order from the order_list.
+                self.order_list.pop(idx)
+                self.number_of_orders -= 1
+                order_list_length = max(order_list_length - 1, 0)
+            
+            elif self.order_list[idx]._quantity_release_needed():
+                # If the order isn't completely filled, release the disclosed quantity and push the order to the end of the order_list.
+                order = self.order_list.pop(idx)
+                order._release_quantity()
+                self.order_list.append(order)
+                order_list_length = max(order_list_length - 1, 0)
+            else:
+                idx += 1
+        return
+    
+    def is_empty(self):
+        return self.order_list == []
+
+    def __str__(self) -> str:
+        if self.order_list == []:
+            print("Order Level Empty")
+        order_attribute_list = ['arrival_time', 'direction', 'price', 'display_quantity']
+        for attribute in order_attribute_list:
+            print(attribute, end=' ')
+        for order in self.order_list:
+            for attribute in order_attribute_list:
+                print(f"{order.__getattribute__(attribute)}", end=' ')
+            print('\n')
+        return
---
+++ src/orders.py
@@ -0,0 +1,135 @@
+import datetime as dt
+from random import randint
+
+class Order:
+    order_request_contents = {
+        'client_id': int,
+        'custom_id': str
+        }
+
+    def __init__(self) -> None:
+        self.client_id = None
+        self.custom_id = None
+        self.order_id = None
+        self.arrival_time = dt.datetime.now()
+    
+    def _verify_and_set_order_request(self, order_request_dict, order_request_contents=None):
+        if not order_request_contents:
+            order_request_contents = self.order_request_contents
+
+        for key in order_request_contents:
+            if key in order_request_dict:
+                if isinstance(order_request_dict[key], order_request_contents[key]):
+                    self.__setattr__(key, order_request_dict[key])
+                else:
+                    raise TypeError(f"'{key}' field must have type {order_request_contents[key]} \
+                        but Invalid type passed : {order_request_dict[key]}")
+            else:
+                raise Exception(f"{key} field not present in the request.")
+        
+        self._set_order(order_request_dict)
+        return
+    
+    def _set_order(self, order_dict):
+        """
+        Set the class attributes.
+        """
+        for key, value in order_dict.items():
+            self.__setattr__(key, value)
+        return
+        
+
+class NewOrder(Order):
+    """
+    This is an order object present at a particular level. This has all the neccessary data regarding a particular order placed 
+    by a particular client.
+    """
+    order_request_contents = {
+        'order_type': str,
+        'direction': str,
+        'instrument_name': str,
+        'price': float,
+        'total_quantity': int,
+        'disclosed_quantity': int
+    }
+    order_type_choices = ['LIMIT', 'MARKET']
+    direction_choices = ['BUY', 'SELL']
+
+    def __init__(self, order_request_dict) -> None:
+        super().__init__()
+        self.order_purpose = 'NEW'
+        self.order_type = None
+        self.direction = None
+        self.instrument_name = None
+        self.price = None
+        self.total_quantity = None
+        self.disclosed_quantity = None
+
+        self.filled_quantity = 0
+        self.display_quantity = None
+        super()._verify_and_set_order_request(order_request_dict)
+        self._verify_and_set_order_request(order_request_dict)
+
+    def _verify_and_set_custom_attributes(self):
+        """
+        Verifies the order request and populates the class attributes.
+
+        ###############################################################################################
+        Need to seperate the 'verify' and 'set' parts.
+        ###############################################################################################
+        """
+        # Quantity check
+        if self.disclosed_quantity > self.total_quantity:
+            raise ValueError(f"disclosed_quantity({self.disclosed_quantity}) must be less \
+                than or equal to total_quantity({self.total_quantity})")
+
+        # Quantity to show in the orderbook.
+        self.display_quantity = self.disclosed_quantity
+        self.order_id = randint(1, 1000000)
+
+        # Field specific checks
+        for attribute in ['order_type', 'direction']:
+            attribute_value = self.__getattribute__(attribute)
+            if attribute_value not in self.__getattribute__(f"{attribute}_choices"):
+                raise ValueError(f"'{attribute_value}' not a valid value for the field {attribute}")
+
+        for attribute in ['price', 'total_quantity', 'disclosed_quantity']:
+            attribute_value = self.__getattribute__(attribute)
+            if attribute_value <= 0:
+                raise ValueError(f"Value of field {attribute} must be non-zero. Instead it is '{attribute_value}'")
+            
+        ############################### Add check for instrument_name based on regex ######################################
+        #################################### Add check for arrival_time based on regex ########################################
+        return
+    
+    def _release_quantity(self):
+        """
+        Release more quantity based on the disclosed quantity as and when the display quantity is filled.
+        """
+        self.display_quantity = min(self.total_quantity - self.filled_quantity, self.disclosed_quantity)
+        return
+
+    def _update_filled_quantity(self, filled_quantity):
+        """
+        Update the filled quantity for this order.
+        """
+        self.filled_quantity += filled_quantity
+        self.display_quantity -= filled_quantity
+        return
+    
+    def _is_filled(self):
+        return self.filled_quantity == self.total_quantity
+    
+    def _quantity_release_needed(self):
+        return self.display_quantity == 0 and self.filled_quantity != 0
+
+class CancelOrder(Order):
+    order_request_contents = {
+        'order_id': int
+    }
+
+    def __init__(self, order_request_dict) -> None:
+        super().__init__()
+        self.order_purpose = 'CANCEL'
+        super()._verify_and_set_order_request(order_request_dict)
+        self._verify_and_set_order_request(order_request_dict)
